0819ebf9eac60b692e20f13c3cabd39913d3b991
Thomas Haugland Rudfoss <thomas.rudfoss@bouvet.no>
2023-11-01 17:04:55 +0100

    useState

--- apps/useradmin-wfe/src/HelloWorld.tsx
+++ apps/useradmin-wfe/src/HelloWorld.tsx
@@ -1,8 +1,21 @@
-export function HelloWorld() {
+import { useState } from "react"
+
+export const HelloWorld = () => {
+	const [inputValue, setInputValue] = useState("hei!🚀")
+
+	const onInputChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
+		setInputValue(evt.target.value)
+	}
+
 	return (
 		<div>
 			<label htmlFor="test">Label</label>
-			<input id="test" type="text" />
+			<input
+				id="test"
+				value={inputValue}
+				onChange={onInputChange}
+				type="text"
+			/>
 		</div>
 	)
 }
