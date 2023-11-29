473bfa0ed7988c9bc731dc4b37e800b91748c544
Thomas Haugland Rudfoss <thomas.rudfoss@bouvet.no>
2023-11-29 14:44:27 +0100

    Bypass limit

--- apps/store-front/src/ClickUntil/ClickUntil.tsx
+++ apps/store-front/src/ClickUntil/ClickUntil.tsx
@@ -3,13 +3,13 @@ import { useState } from "react"
 export interface ClickUntilProps {
 	message?: string
 	limit?: number
-	skipLimit?: boolean
+	bypassLimit?: boolean
 }
 
 export const ClickUntil = ({
 	message = "Limit reached",
 	limit = 10,
-	skipLimit = false
+	bypassLimit = false
 }: ClickUntilProps) => {
 	const [count, setCount] = useState(0)
 	const isLimitReached = count >= limit
@@ -23,12 +23,12 @@ export const ClickUntil = ({
 
 	return (
 		<>
-			<button onClick={increment} disabled={isLimitReached && !skipLimit}>
+			<button onClick={increment} disabled={isLimitReached && !bypassLimit}>
 				Click me!
 			</button>
 			<p>
-				{isLimitReached && !skipLimit && message}
-				{isLimitReached && skipLimit && `${message} - ${count}`}
+				{isLimitReached && !bypassLimit && message}
+				{isLimitReached && bypassLimit && `${message} - ${count}`}
 				{!isLimitReached && count}
 			</p>
 			{isLimitReached && <button onClick={resetCount}>Reset</button>}
--- apps/store-front/src/ClickUntil/ClickUntilForm.tsx
+++ apps/store-front/src/ClickUntil/ClickUntilForm.tsx
@@ -9,18 +9,18 @@ export interface ClickUntilFormProps {}
 export const ClickUntilForm = (props: ClickUntilFormProps) => {
 	const [message, setMessage] = useState("Limit reached")
 	const [limit, setLimit] = useState(15)
-	const [skipLimit, setSkipLimit] = useState(false)
+	const [bypassLimit, setBypassLimit] = useState(false)
 
 	return (
 		<div>
 			<TextField label="Message" value={message} onChange={setMessage} />
 			<NumericField label="Limit" value={limit} onChange={setLimit} />
 			<BooleanField
-				label="Skip limit"
-				value={skipLimit}
-				onChange={setSkipLimit}
+				label="Bypass limit"
+				value={bypassLimit}
+				onChange={setBypassLimit}
 			/>
-			<ClickUntil message={message} limit={limit} skipLimit={skipLimit} />
+			<ClickUntil message={message} limit={limit} bypassLimit={bypassLimit} />
 		</div>
 	)
 }
