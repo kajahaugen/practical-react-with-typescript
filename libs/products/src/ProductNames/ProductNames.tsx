85691777ed40ebad8c54afe688cbd05e2d2fda51
Thomas Haugland Rudfoss <thomas.rudfoss@bouvet.no>
2023-11-30 10:36:46 +0100

    Updated product names

--- libs/products/src/ProductNames/ProductNames.tsx
+++ libs/products/src/ProductNames/ProductNames.tsx
@@ -1,25 +1,60 @@
-import { useState } from "react"
+import { useMemo, useState, MouseEvent } from "react"
 
 import { Product } from "../products"
 
-export interface ProductNamesProps {
-	products: Product[]
+const getSortDirectionText = (sortDirection?: "asc" | "desc") => {
+	switch (sortDirection) {
+		case undefined:
+			return "none"
+		case "asc":
+			return "ascending"
+		case "desc":
+			return "descending"
+	}
 }
 
-export const ProductNames = ({ products }: ProductNamesProps) => {
+export interface ProductNamesProps {
+	products: Product[]
+	onDelete?: (product: Product) => unknown
+}
+
+export const ProductNames = ({ products, onDelete }: ProductNamesProps) => {
 	const [selectedProduct, setSelectedProduct] = useState<{
 		product: Product
 		idx: number
 	}>()
+	const [sortDirection, setSortDirection] = useState<"asc" | "desc">()
+	const sortedProducts = useMemo(() => {
+		if (!sortDirection) return products
+
+		const presortedProducts = products.slice(0)
+		presortedProducts.sort((a, b) => {
+			const result = a.title.localeCompare(b.title)
+			return sortDirection === "asc" ? result : result * -1
+		})
+
+		return presortedProducts
+	}, [products, sortDirection])
 
 	const selectProduct = (product: Product, idx: number) => () => {
 		setSelectedProduct({ product, idx })
+	}
+	const deleteProduct =
+		(product: Product) => (evt: MouseEvent<HTMLButtonElement>) => {
+			evt.stopPropagation()
+			onDelete?.(product)
+		}
+	const toggleSortDirection = () => {
+		setSortDirection(sortDirection === "desc" ? "asc" : "desc")
 	}
 
 	return (
 		<>
 			<h3>List of products</h3>
 			<p>There are {products.length} product(s)</p>
+			<button onClick={toggleSortDirection}>
+				Sort direction {getSortDirectionText(sortDirection)}
+			</button>
 			{selectedProduct && (
 				<p>
 					<strong>Selected product {selectedProduct.idx}:</strong>{" "}
@@ -27,13 +62,14 @@ export const ProductNames = ({ products }: ProductNamesProps) => {
 				</p>
 			)}
 			<ul>
-				{products.map((product, idx) => (
+				{sortedProducts.map((product, idx) => (
 					<li
 						key={product.id}
 						onClick={selectProduct(product, idx)}
 						style={{ cursor: "pointer" }}
 					>
 						{product.title}
+						<button onClick={deleteProduct(product)}>Delete</button>
 					</li>
 				))}
 			</ul>
